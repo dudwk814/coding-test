42a35947112732ee2c058d8d2f9cfdd3ecb214f2
dudwk814 <kj99658103@gmail.com>
2021-12-27 22:52:59 +0900

    2021-12-27 DFS/BFS

---
+++ 알고리즘/자료구조/스택(Stack).py
@@ -0,0 +1,15 @@
+# 스택 : 선입후출의 구조를 가진 자료구조
+
+stack = []
+
+stack.append(5)
+stack.append(2)
+stack.append(3)
+stack.append(7)
+stack.pop()
+stack.append(1)
+stack.append(4)
+stack.pop()
+
+print(stack)  # 최하단 원소부터 출력
+print(stack[::-1])
---
+++ 알고리즘/자료구조/팩토리얼(factorial).py
@@ -0,0 +1,20 @@
+# 반복적으로 구현한 n!
+def factorial_iterative(n):
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+
+# 재귀적으로 구현한 n!
+
+
+def factorial_recursive(n):
+    if n <= 1:  # n이 1 이하인 경우 1을 반환
+        return 1
+    # n! = n * (n - 1)!를 그래도 코드로 작성하기
+    return n * factorial_recursive(n - 1)
+
+
+# 각자의 방식으로 구현한 n! 출력(n = 5)
+print('반복적으로 구현:', factorial_iterative(5))
+print('재귀적으로 구현:', factorial_recursive(5))
