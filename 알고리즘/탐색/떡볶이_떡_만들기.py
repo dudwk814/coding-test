d1b16faae7c94d7afb1aeb05946187bcacd0769b
dudwk814 <kj99658103@gmail.com>
2022-01-03 23:55:58 +0900

    2022-01-03 다이나믹프로그래밍1

--- 알고리즘/탐색/떡볶이_떡_만들기.py
+++ 알고리즘/탐색/떡볶이_떡_만들기.py
@@ -17,13 +17,13 @@ while (start <= end):
         if x > mid:
             total += x - mid
 
-        # 떡의 양이 부족한 경우 더 많이 자르기 (왼쪽 부분 탐색)
-        if total < m:
-            end = mid - 1
-        # 떡의 양이 충분한 경우 덜 자르기(오른쪽 부분 탐색)
-        else:
-            result = mid
-            start = mid + 1
+     # 떡의 양이 부족한 경우 더 많이 자르기 (왼쪽 부분 탐색)
+    if total < m:
+        end = mid - 1
+    # 떡의 양이 충분한 경우 덜 자르기(오른쪽 부분 탐색)
+    else:
+        result = mid
+        start = mid + 1
 
 # 정답 출력
 print(result)
