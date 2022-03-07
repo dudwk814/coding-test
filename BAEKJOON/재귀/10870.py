fe6643e86c5490f6455a4aa3b348abb8abdce284
dudwk814 <kj99658103@gmail.com>
2022-03-07 22:26:25 +0900

    2022-03-07

---
+++ BAEKJOON/재귀/10870.py
@@ -0,0 +1,19 @@
+n = int(input())
+d = [0] * 100
+
+
+def fibo(x):
+    if x == 1 or x == 2:
+        return 1
+
+    if d[x] != 0:
+        return d[x]
+
+    d[x] = fibo(x - 1) + fibo(x - 2)
+    return d[x]
+
+
+if n == 0:
+    print(0)
+else:
+    print(fibo(n))
