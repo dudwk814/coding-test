c43209832698d1d359d76a42b7118edbdfdea78b
dudwk814 <kj99658103@gmail.com>
2022-04-08 19:55:47 +0900

    2000

--- BAEKJOON/DFS&BFS/7576.py
+++ BAEKJOON/DFS&BFS/7576.py
@@ -1,64 +1,43 @@
 from collections import deque
 
 m, n = map(int, input().split())
-
-count = 0
-zero_count = 0
-
-graph = []
-
+queue = deque([])
+graph = [list(map(int, input().split())) for _ in range(n)]
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
-
-for i in range(n):
-    input_data = list(map(int, input().split()))
-    graph.append(input_data)
-
-print(*graph)
-
-
-def bfs(x, y):
-
-    global count
-
-    queue = deque()
-
-    queue.append((x, y))
-
-    while queue:
-        x, y = queue.popleft()
-
-        count += 1
-
-        for i in range(4):
-
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-            if nx < 0 or ny < 0 or nx >= n or ny >= m:
-                continue
-
-            if graph[nx][ny] == -1:
-                continue
-
-            if graph[nx][ny] == 0:
-                graph[nx][ny] = 2
-                queue.append((nx, ny))
-
+result = 0
 
 for i in range(n):
     for j in range(m):
         if graph[i][j] == 1:
-            bfs(i, j)
+            queue.append([i, j])
+
+
+def bfs():
+    while queue:
+        x, y = queue.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                continue
+
+            if graph[nx][ny] != 0:
+                continue
+
+            graph[nx][ny] = graph[x][y] + 1
+            queue.append([nx, ny])
+
+
+bfs()
 
 for i in range(n):
     for j in range(m):
         if graph[i][j] == 0:
-            zero_count += 1
+            print(-1)
+            exit(0)
+        result = max(result, graph[i][j])
 
-if zero_count == 0:
-    print(count)
-else:
-    print(-1)
-
-print(* graph)
+print(result - 1)
