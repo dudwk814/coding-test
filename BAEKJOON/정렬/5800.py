4e3b52bed1f8023ba70587e15c13ef177f5cfded
dudwk814 <kj99658103@gmail.com>
2022-08-13 19:52:25 +0900

    2022-08-13

---
+++ BAEKJOON/정렬/5800.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+for i in range(n):
+    score = list(map(int, input().split()))
+    score.remove(score[0])
+    max_score = max(score)
+    min_score = min(score)
+    gap = 0
+
+    score.sort(reverse=True)
+
+    for j in range(len(score) - 1):
+        gap = max(gap, score[j] - score[j + 1])
+
+    print("Class", i + 1)
+    print(f"Max {max_score}, Min {min_score}, Largest gap {gap}")
